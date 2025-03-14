d23062cc45edad5bd5676092a75ebf9b8a23a745
Willie  Conway <85038615+Willie-Conway@users.noreply.github.com>
2025-03-14 14:49:37 -0400

    Update views.py

--- concert/views.py
+++ concert/views.py
@@ -1,3 +1,4 @@
+import requests
 from django.contrib.auth import login, logout
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect, HttpResponse
@@ -6,6 +7,10 @@ from django.urls import reverse
 from django.contrib.auth.hashers import make_password
 from concert.forms import LoginForm, SignUpForm
 from concert.models import Concert, ConcertAttending
+
+# URL configurations for the Songs and Photos microservices
+SONGS_URL = "http://songs-sn-labs-hirewillieco.labs-prod-openshift-san-a45631dc5778dc6371c67d206ba9ae5c-0000.us-east.containers.appdomain.cloud"  # Songs microservice URL
+PHOTO_URL = "https://pictures.1t07bpn0l6uk.us-south.codeengine.appdomain.cloud"  # Photos microservice URL
 
 # Create your views here.
 
@@ -16,7 +21,7 @@ def signup(request):
         try:
             user = User.objects.filter(username=username).first()
             if user:
-                return render(request, "signup.html", {"form": SignUpForm, "message": "user already exist"})
+                return render(request, "signup.html", {"form": SignUpForm, "message": "user already exists"})
             else:
                 user = User.objects.create(username=username, password=make_password(password))
                 login(request, user)
@@ -29,33 +34,13 @@ def index(request):
     return render(request, "index.html")
 
 def songs(request):
-    # Dummy data for songs
-    songs = {
-        "songs": [
-            {
-                "id": 1,
-                "title": "duis faucibus accumsan odio curabitur convallis",
-                "lyrics": "Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis."
-            },
-            {
-                "id": 2,
-                "title": "Lorem ipsum dolor sit amet",
-                "lyrics": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam."
-            }
-        ]
-    }
+    # Use the SONGS_URL to fetch songs from the Songs microservice
+    songs = requests.get(f"{SONGS_URL}/song").json()
     return render(request, "songs.html", {"songs": songs["songs"]})
 
 def photos(request):
-    # Dummy data for photos
-    photos = [{
-        "id": 1,
-        "pic_url": "http://dummyimage.com/136x100.png/5fa2dd/ffffff",
-        "event_country": "United States",
-        "event_state": "District of Columbia",
-        "event_city": "Washington",
-        "event_date": "11/16/2022"
-    }]
+    # Use the PHOTO_URL to fetch photos from the Photos microservice
+    photos = requests.get(f"{PHOTO_URL}/picture").json()
     return render(request, "photos.html", {"photos": photos})
 
 def login_view(request):
